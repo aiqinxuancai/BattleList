3ad22d73cb8caf48fb20a589d5caefe17147a179
爱琴炫彩 <aiqinxuancai@163.com>
2018-03-06 18:50:10 +0800

    update

--- BattleList/BattleList.cs
+++ BattleList/BattleList.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,9 @@ using System.Threading.Tasks;
 
 namespace BattleList
 {
+
+
+
     class BattleList
     {
         private static readonly BattleList instance = new BattleList();
@@ -18,11 +22,11 @@ namespace BattleList
         {
             get { return instance; }
         }
-
-
         private static string JSON_PATH = @".\Data\BattleList";
-
         private Plugin plugin;
+
+
+        private JObject m_lastStart, m_lastBattleResult;
 
 
         public void Initialize(Plugin plugin)
@@ -32,18 +36,54 @@ namespace BattleList
             this.plugin = plugin;
 
             APIObserver o = APIObserver.Instance;
-            o["api_req_sortie/battleresult"].ResponseReceived += OnBattleResultCompleted;
-            o["api_req_combined_battle/battleresult"].ResponseReceived += OnCombinedBattleResultCompleted;
+
+            o["api_req_map/start"].ResponseReceived += OnMapStart;
+            o["api_req_map/next"].ResponseReceived += OnMapNext;
+            o["api_req_sortie/battleresult"].ResponseReceived += OnBattleResult;
+            o["api_req_combined_battle/battleresult"].ResponseReceived += OnCombinedBattleResult;
         }
 
 
-        private void OnBattleResultCompleted(string apiname, dynamic data)
+        private void OnMapStart(string apiname, dynamic data)
+        {
+            EasyLogOut.Write("Plugin:OnMapStart");
+            Codeplex.Data.DynamicJson json = data;
+            EasyLogOut.Write(data);
+            try
+            {
+                JObject root = JObject.Parse(json.ToString());
+                m_lastStart = root;
+            }
+            catch (System.Exception ex)
+            {
+                EasyLogOut.Write(ex);
+            }
+        }
+
+        private void OnMapNext(string apiname, dynamic data)
+        {
+            EasyLogOut.Write("Plugin:OnMapNext");
+            Codeplex.Data.DynamicJson json = data;
+            EasyLogOut.Write(data);
+            try
+            {
+                JObject root = JObject.Parse(json.ToString());
+                var next = root;
+                if (m_lastStart != null)
+                {
+                    m_lastStart.Merge(next);//合并点数据
+                }
+            }
+            catch (System.Exception ex)
+            {
+                EasyLogOut.Write(ex);
+            }
+        }
+
+        private void OnBattleResult(string apiname, dynamic data)
         {
             EasyLogOut.Write("Plugin:OnBattleResultCompleted");
-
             Codeplex.Data.DynamicJson json = data;
-
-            EasyLogOut.Write(data.GetType());
             EasyLogOut.Write(data);
             try
             {
@@ -56,13 +96,10 @@ namespace BattleList
             }
         }
 
-        private void OnCombinedBattleResultCompleted(string apiname, dynamic data)
+        private void OnCombinedBattleResult(string apiname, dynamic data)
         {
             EasyLogOut.Write("Plugin:OnCombinedBattleResultCompleted");
-
             Codeplex.Data.DynamicJson json = data;
-
-            EasyLogOut.Write(data.GetType());
             EasyLogOut.Write(data);
             try
             {
@@ -74,6 +111,13 @@ namespace BattleList
                 File.WriteAllText(JSON_PATH + @"\CombinedBattleResult" + DateTime.Now.Ticks.ToString() + ".json", ex.ToString());
             }
         }
+
+        private void SaveBattleResult(JObject root)
+        {
+
+        }
+        
+
 
     }
 }
---
+++ BattleList/BattleListCell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleList
+{
+    public class BattleListCell
+    {
+        public DateTime Time { get; set; }
+        public string MapName { get; set; }
+        public int MapPointId { get; set; }
+        public string MapPointName { get; set; }
+        public int WinRankId { get; set; }
+        public string WinRank { get; set; }
+
+        /// <summary>
+        /// 掉落的新船的名字
+        /// </summary>
+        public string NewShipName { get; set; }
+        /// <summary>
+        /// 敌方舰队名称
+        /// </summary>
+        public string DeckName { get; set; }
+
+
+
+
+
+    }
+}
